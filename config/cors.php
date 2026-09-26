665184260f7db82965a58d41ce21bb6a63b3213b
Shahn Moyshan <freelance.shain@gmail.com>
2026-09-26 13:12:31 +0600

    feat: add CORS configuration and storage settings for file management

--- config/cors.php
+++ config/cors.php
@@ -1,21 +1,27 @@
 <?php
 /**
+ * CORS configuration file.
+ * 
+ * The specified paths will be matched against the request path. 
+ * @var array [] = all paths and ['api/*', 'admin/*'] = specific paths
+ * 
  * The allowed origin. An asterisk (*) is a wildcard character that will match all origins.
  * @var string|array string: '*' or array:['https://example.com', ...]
- *
+ * 
  * The methods, and headers are also specified in this configuration file.
  * @var array
- *
+ * 
  * The credentials option indicates whether the response to the request can be exposed when the credentials flag is true.
  * @var bool
- *
+ * 
  * The age option indicates how long the results of a preflight request can be cached in a preflight result cache.
  * @var int
- *
+ * 
  * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
  */
 
 return [
+    'paths' => [],
     'origin' => '*',
     'methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     'headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'X-XSRF-TOKEN'],
--- config/database.php
+++ config/database.php
@@ -5,9 +5,10 @@ return [
     'connections' => [
         'sqlite' => [
             'driver' => 'sqlite',
-            'file' => dirname(__DIR__) . '/database/sqlite.db', // SQLite Database filepath
+            'file' => dirname(__DIR__) . '/database/sqlite.db', // SQLite Database filepath 
         ],
         'default' => [
+            // 'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'name' => env('DB_DATABASE', 'spark'),
